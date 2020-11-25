83a535ade10c2b703119d6e34a01772dcaad0d95
Dave Brock <daveabrock@gmail.com>
2020-11-25 10:55:07 -0600

    Add project files.

---
+++ MassImageUploader/Image.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MassImageUploader
+{
+    public class Image
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("copyright")]
+        public string Copyright { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+        [JsonProperty("explanation")]
+
+        public string Explanation { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
---
+++ MassImageUploader/ImageUploader.cs
@@ -0,0 +1,81 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Collections.Generic;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using System.Linq;
+using Microsoft.Azure.Cosmos;
+
+namespace MassImageUploader
+{
+    public class ImageUploader
+    {
+        private readonly HttpClient httpClient;
+        private CosmosClient cosmosClient;
+        private BlobClient blobClient;
+
+        public ImageUploader()
+        {
+            httpClient = new HttpClient();
+            cosmosClient = new CosmosClient(Environment.GetEnvironmentVariable("CosmosEndpoint"),
+                                            Environment.GetEnvironmentVariable("CosmosKey"));
+        }
+
+        [FunctionName("Uploader")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "upload")] HttpRequest req,
+            ILogger log)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("ApiKey");
+
+            var response = await httpClient.GetAsync($"https://api.nasa.gov/planetary/apod?api_key={apiKey}");
+            var result = await response.Content.ReadAsStringAsync();
+
+            var imageDetails = JsonConvert.DeserializeObject<Image>(result);
+
+            await UploadImageToAzureStorage(imageDetails.Url);
+            await AddImageToContainer(imageDetails);
+            return new OkObjectResult("Processing complete.");
+        }
+
+        private async Task<bool> UploadImageToAzureStorage(string imageUri)
+        {
+            var fileName = GetFileNameFromUrl(imageUri);
+            var blobUri = new Uri($"{Environment.GetEnvironmentVariable("BlobContainerUrl")}/{fileName}");
+            var storageCredentials = new StorageSharedKeyCredential(
+                    Environment.GetEnvironmentVariable("StorageAccount"),
+                    Environment.GetEnvironmentVariable("StorageKey"));
+            var blobClient = new BlobClient(blobUri, storageCredentials);
+            await blobClient.StartCopyFromUriAsync(new Uri(imageUri));
+            return await Task.FromResult(true);
+        }
+
+        private async Task<bool> AddImageToContainer(Image image)
+        {
+            var container = cosmosClient.GetContainer(Environment.GetEnvironmentVariable("CosmosDatabase"),
+                                                      Environment.GetEnvironmentVariable("CosmosContainer"));
+
+            var fileName = GetFileNameFromUrl(image.Url);
+
+            image.Id = Guid.NewGuid();
+            image.Url = $"{Environment.GetEnvironmentVariable("BlobContainerUrl")}/{fileName}";
+
+            await container.CreateItemAsync(image);
+            return await Task.FromResult(true);
+        }
+
+        private string GetFileNameFromUrl(string urlString)
+        {
+            var url = new Uri(urlString);
+            return url.Segments.Last();
+        }
+    }
+}
